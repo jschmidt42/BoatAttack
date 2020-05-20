b77c124c05b8cf96946b7c86b204449f65ee4716
Jonathan Schmidt <jonathans@unity3d.com>
2020-05-20 09:20:48 -0400

    Quick search setup

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [Tooltip("English 简体中文 わぶん 한국어")]
+    [Header("English")]
+    public string english;
+    [Header("简体中文")]
+    public string chinese;
+    [Header("わぶん")]
+    public string japanese;
+    [Header("한국어")]
+    public string korean;
+}
